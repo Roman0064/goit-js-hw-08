54c946ba9ece770031e3fbe40e29e9ec46bc6429
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-05-25 13:30:39 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,30 @@
-import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
 // Change code below this line
-const bodyEl = document.querySelector("#root");
 
 const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-const galleryItem = galleryItems.map(({ preview, original, description }) => {
-  const galleryList = `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-    </a>
-  </li>`;
-  return galleryList;
-});
-galleryContainer.insertAdjacentHTML("beforeend", galleryItem.join(""));
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  }).join('');
+}
 
-let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+// use library SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
 });
