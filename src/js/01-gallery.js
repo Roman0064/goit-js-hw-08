aa89800736c1b10cf82779a60501785230fffdd5
Roman0064 <122211211+Roman0064@users.noreply.github.com>
2023-05-25 13:01:47 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Change code below this line
+const bodyEl = document.querySelector("#root");
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItemMarkup(item) {
+  const { preview, original, description } = item;
+
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}
+
+function renderGalleryItems(items) {
+  const galleryItemsMarkup = items.map(createGalleryItemMarkup).join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+}
+
+renderGalleryItems(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
